26372a4bade30a475a6657b84c72233445baeb28
TomatoDev <diredarknil@gmail.com>
2019-03-16 21:39:02 -0500

    new xamarin items
    Signed-off-by: TomatoDev <diredarknil@gmail.com>

---
+++ MealMelt/Adapters/RecipeAdapterViewHolder.cs
@@ -0,0 +1,18 @@
+﻿using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+
+namespace MealMelt
+{
+    class RecipeAdapterViewHolder : RecyclerView.ViewHolder
+    {
+        public ImageView Image { get; private set; }
+        public TextView Caption { get; private set; }
+
+        public RecipeAdapterViewHolder(View itemView) : base(itemView)
+        {
+            Image = itemView.FindViewById<ImageView>(Resource.Id.recipeImage);
+            Caption = itemView.FindViewById<TextView>(Resource.Id.txtTitle);
+        }
+    }
+}
---
+++ MealMelt/Adapters/RecipePagerAdapter.cs
@@ -0,0 +1,27 @@
+﻿using Android.Support.V4.App;
+
+namespace MealMelt
+{
+    class RecipePagerAdapter : FragmentPagerAdapter
+    {
+        private readonly Fragment[] _fragments;
+
+        public RecipePagerAdapter(FragmentManager fm, params Fragment[] fragments) : base(fm)
+        {
+            _fragments = fragments;
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return _fragments.Length;
+            }
+        }
+
+        public override Fragment GetItem(int position)
+        {
+            return _fragments[position];
+        }
+    }
+}
